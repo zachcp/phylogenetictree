464991fe0f23850fc465c20fd400434cecabc53a
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-02-15 18:18:57 -0500

    init phylogenetic tree

---
+++ R/phylogenetictree.R
@@ -0,0 +1,40 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+phylogenetictree <- function(tree, outerradius = 480, innerradius = 310, width = NULL, height = NULL) {
+
+  # forward options using x
+  params = list(
+    tree = tree,
+    outerradius = outerradius,
+    innerradius = innerradius
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'phylogenetictree',
+    x = params,
+    width = width,
+    height = height,
+    package = 'phylogenetictree'
+  )
+}
+
+#' Widget output function for use in Shiny
+#'
+#' @export
+phylogenetictreeOutput <- function(outputId, width = '100%', height = '400px'){
+  shinyWidgetOutput(outputId, 'phylogenetictree', width, height, package = 'phylogenetictree')
+}
+
+#' Widget render function for use in Shiny
+#'
+#' @export
+renderPhylogenetictree <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  shinyRenderWidget(expr, phylogenetictreeOutput, env, quoted = TRUE)
+}
